6bd9bb35ce2d397adbac6b0b0fbad646dcb9e434
Islam Shalaby <islamshalby510@gmail.com>
2021-12-27 15:08:19 +0200

    edits 27/12/2021 v2

--- app/Category.php
+++ app/Category.php
@@ -13,11 +13,11 @@ class Category extends Model
     }
 
     public function subCats() {
-        return $this->hasMany('App\SubCategory', 'category_id')->select('id', 'image')->where('deleted', 0)->where('is_show', 1);
+        return $this->hasMany('App\SubCategory', 'category_id')->select('id', 'image')->where('deleted', 0);
     }
 
     public function subCategories($lang) {
-        return $this->hasMany('App\SubCategory', 'category_id')->select('id', 'image', 'title_' . $lang . ' as title')->where('deleted', 0);
+        return $this->hasMany('App\SubCategory', 'category_id')->select('id', 'image', 'title_' . $lang . ' as title')->where('deleted', 0)->where('is_show', 1);
     }
 
     public function plans() {
